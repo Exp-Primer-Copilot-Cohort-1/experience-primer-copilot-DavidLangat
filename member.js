56506db1e9e1957463501b7c91b0012559c9424a
David <93221294+DavidLangat@users.noreply.github.com>
2023-12-13 15:58:56 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,91 @@
+function skillsMember() {
+    var skills = {
+        "skills": [
+            {
+                "id": "1",
+                "name": "Javascript",
+                "img": "img/javascript.png",
+                "level": "80"
+            },
+            {
+                "id": "2",
+                "name": "HTML5",
+                "img": "img/html5.png",
+                "level": "90"
+            },
+            {
+                "id": "3",
+                "name": "CSS3",
+                "img": "img/css3.png",
+                "level": "90"
+            },
+            {
+                "id": "4",
+                "name": "jQuery",
+                "img": "img/jquery.png",
+                "level": "80"
+            },
+            {
+                "id": "5",
+                "name": "Bootstrap",
+                "img": "img/bootstrap.png",
+                "level": "80"
+            },
+            {
+                "id": "6",
+                "name": "PHP",
+                "img": "img/php.png",
+                "level": "50"
+            },
+            {
+                "id": "7",
+                "name": "MySQL",
+                "img": "img/mysql.png",
+                "level": "50"
+            },
+            {
+                "id": "8",
+                "name": "Java",
+                "img": "img/java.png",
+                "level": "50"
+            },
+            {
+                "id": "9",
+                "name": "C#",
+                "img": "img/csharp.png",
+                "level": "50"
+            },
+            {
+                "id": "10",
+                "name": "C",
+                "img": "img/c.png",
+                "level": "50"
+            },
+            {
+                "id": "11",
+                "name": "C++",
+                "img": "img/cplusplus.png",
+                "level": "50"
+            },
+            {
+                "id": "12",
+                "name": "Python",
+                "img": "img/python.png",
+                "level": "50"
+            },
+            {
+                "id": "13",
+                "name": "Ruby",
+                "img": "img/ruby.png",
+                "level": "50"
+            },
+            {
+                "id": "14",
+                "name": "React",
+                "img": "img/react.png",
+                "level": "50"
+            },
+        ]
+    };
+    return skills;
+}
